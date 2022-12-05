5df1656e9f287599721ed403a62936256e1225c5
Kashish Sharma <116347828+kvshxsh@users.noreply.github.com>
2022-12-05 18:42:32 +0530

    Update omelet.py

--- omelet.py
+++ omelet.py
@@ -1,3 +1,5 @@
+import time
+
 eggs = float(input("Input the number of eggs the omelet contains: "))
 bread = float(input("Input the number of bread loaves the omelet contains: "))
 
@@ -5,4 +7,10 @@ protein_eggs = 6 #grams
 protein_bread = 2 #grams
 protein_omelet = (eggs*protein_eggs) + (bread*protein_bread)
 
-print("The omelet has %0.1f grams of protein"%(protein_omelet)) #grams
+print("\nThe omelet has %0.1f grams of protein"%(protein_omelet)) #grams
+
+time.sleep(1)
+
+print("\nUntil next time, bye!")
+
+time.sleep(2)
